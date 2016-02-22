445560e1cb7cbdef9860769183b40707f91e1f1f
Broenne <marcus.broenstrup@gmail.com>
2016-02-22 20:31:43 +0100

    no message

---
+++ CheckkerApp/CheckkerApp/www/js/controllers.js
@@ -0,0 +1,32 @@
+var app=angular.module('starter.controllers', []);
+
+
+app.controller('CheckkerCtrl', function($scope) {
+    alert("hello");
+    console.log("hello");
+    $scope.meterNumberInput="";
+    $scope.MeterNumberToAscii = function (data){
+        
+        console.log(data);
+    }
+});
+
+/*.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
+*/
