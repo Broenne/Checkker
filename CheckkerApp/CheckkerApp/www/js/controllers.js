54ddf624ed92660b95d326c989c09d4f5ae7463c
Broenne <marcus.broenstrup@gmail.com>
2016-02-23 20:06:52 +0100

    no message

--- CheckkerApp/CheckkerApp/www/js/controllers.js
+++ CheckkerApp/CheckkerApp/www/js/controllers.js
@@ -8,32 +8,40 @@ app.controller('CheckkerCtrl', function ($scope) {
     $scope.MeterNumberToAscii = function (meterNumberInput, meterVolumeInput) {
         
         // todo mb:
-        meterNumberInput = 12345678;
+        meterNumberInput = "12345678";
         meterVolumeInput = 0;
         var meterNumberAsAscii = new Array();
         angular.forEach(meterNumberInput, function (value, key) {
+			console.log("meter number input:" + key + ': ' + value);
             var ascii = value.charCodeAt(0); // ascii 0 ist dezimal 48, 1=49,...
             meterNumberAsAscii.push(ascii);
         });
 
+		
+		angular.forEach(meterNumberAsAscii, function (value, key) {
+            console.log("helper original:" + key + ': ' + value);
+        });
+
+		
+		
         var meterNumberSum = 0;
         angular.forEach(meterNumberAsAscii, function (value, key) {
             console.log("as ascii original:" + key + ': ' + value);
             // doing modulo 9
-            var modulo9Result = value % 9;
+            var modulo9Result = parseInt(value % 9);
             //meterNoSum += meterNoRemainder[i] * Convert.ToInt32(Math.Pow(2, i));
             var positionPriotity = Math.pow(2, key);
             positionPriotity = parseInt(positionPriotity);
             console.log("positionPriotrity:" + positionPriotity);
-            meterNumberSum = modulo9Result * positionPriotity;
+            meterNumberSum += parseInt(modulo9Result * positionPriotity);
         });
         console.log("meterNumberSum:" + meterNumberSum);
         
         // chekkerRole1 is the low part (right)
-        var chekkerRole1 = meterNumberSum % 9 + 1;
+        var chekkerRole1 = parseInt(meterNumberSum % 9) + 1;
         console.log("chekkerRole1:" + chekkerRole1);
-        meterNumberSum = meterNumberSum / 11;
-        var chekkerRole2 = meterNumberSum % 9 + 1;
+        meterNumberSum = parseInt(meterNumberSum / 11);
+        var chekkerRole2 = parseInt(meterNumberSum % 9) + 1;
         chekkerRole2 = parseInt(chekkerRole2); // mb: hier unterscheidte sich der algorithmus, net parsed automatscih
         console.log("chekkerRole2:" + chekkerRole2);
         
@@ -47,17 +55,17 @@ app.controller('CheckkerCtrl', function ($scope) {
         rolsPos1 += parseInt(meterVolumeInput) + 1;
         console.log("rolsPos1: " + rolsPos1);
 
-        var quotient1 = rolsPos1 / 9;
+        var quotient1 = parseInt(rolsPos1 / 9);
         console.log("quotient1: " + quotient1);
-        var remainder1 = rolsPos1 % 9;
+        var remainder1 = parseInt(rolsPos1 % 9);
         console.log("remainder1: " + remainder1);
 
         chekkerRole1 = rols[remainder1];
         console.log("chekkerRole1: " + chekkerRole1);
-        chekkerRole2 = quotient1 + rolsPos2;
+        chekkerRole2 = parseInt(quotient1 + rolsPos2);// todo mb hier anders
         console.log("chekkerRole2: " + chekkerRole2);
 
-        remainder1 = chekkerRole2 % 9; // hier ist remainder 0,1111
+        remainder1 = parseInt(chekkerRole2 % 9); // hier ist remainder 0,1111
         console.log("remainder1: " + remainder1);
         chekkerRole2 = rols[remainder1] * 10;
         console.log("chekkerRole2: " + chekkerRole2);
